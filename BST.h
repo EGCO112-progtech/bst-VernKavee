081bc6d40ab352bc7164d6cc1269d4afddf75e05
VernKavee <156408744+VernKavee@users.noreply.github.com>
2024-03-01 07:56:58 +0000

    save

--- BST.h
+++ BST.h
@@ -1,9 +1,9 @@
 // An integer binary search tree
 
-struct TreeNode {                                           
-    struct TreeNode *leftPtr; // pointer to left subtree
-   int data; // node value                               
-    struct TreeNode *rightPtr; // pointer to right subtree
+struct TreeNode {         
+   int data; // node value                                  
+   struct TreeNode *leftPtr; // pointer to left subtree
+   struct TreeNode *rightPtr; // pointer to right subtree
 }; // end structure treeNode 
         
 typedef struct TreeNode * TreeNodePtr; // synonym for TreeNode*
@@ -14,8 +14,8 @@ typedef struct {
 }BST;
 
 // prototypes
+void insertNode( BST *, int);
 
-void insertNode( BST *, int );
 /*
 void insertNode_R(TreeNodePtr * t,int value){
   if(t==NULL){
@@ -34,35 +34,44 @@ void insertNode_R(TreeNodePtr * t,int value){
    }
 }
 */
+
 void insertNode( BST *b, int value ){
-	 TreeNodePtr t =b->root,new_node;
-	 int inserted=0;
-	new_node =(TreeNodePtr) malloc(sizeof(struct TreeNode));
+	TreeNodePtr t = b->root, new_node;
+	int inserted = 0;
+
+	new_node = (TreeNodePtr) malloc(sizeof(struct TreeNode));
 	if(new_node){
-		new_node->leftPtr=NULL;
-		new_node->rightPtr=NULL;
-		new_node->data=value;
+		new_node->leftPtr = NULL;
+		new_node->rightPtr = NULL;
+		new_node->data = value;
+
 		/*First Node*/
-		if(!b->root)	b->root=new_node;
-		else {
-while(!inserted){
-   if(t->data >=value){
-     /* move/insert to the left*/
-    
-     }
-  	 
+		if(b->size == 0) b->root = new_node;
+
+		else{
+         while(!inserted){
+            if(t->data >= value){
+               if(t->leftPtr != NULL) t = t->leftPtr;
+               else{
+                  t->leftPtr = new_node;
+                  inserted = 1;
+               } 
+               /* move/insert to the left*/
+            }
+
+            else{
+               if(t->rightPtr != NULL) t = t->rightPtr;
+               else{
+                  t->rightPtr = new_node;
+                  inserted = 1;
+               } 
+	            /* move/insert to the right*/
+            }
+            
+         }//end while		
+      }//end else;
+   b->size++;
   }
-   else{
-	      /* move/ insert to the right*/
-    
-    }
-	}
-   
-  }//end while		
-  }//end else;
-  b->size++;
-  }
-  
 }//end function
 
 
@@ -71,10 +80,10 @@ void inOrder( TreeNodePtr treePtr )
    // if tree is not empty, then traverse
    if ( treePtr != NULL ) {        
         
-      inOrder( treePtr->leftPtr ); //Recursion to the left
+      inOrder(treePtr->leftPtr); //Recursion to the left
  
-      printf("%3d",treePtr->data) ;  //print the value 
+      printf("%3d", treePtr->data);  //print the value 
    
-      inOrder( treePtr->rightPtr ); //Recursion to the right
+      inOrder(treePtr->rightPtr); //Recursion to the right
    } // end if                          
 } // end 
--- main.c
+++ main.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "BST.h"
-int main( int argc, char** argv )
+
+int main(int argc, char **argv)
 { 
    unsigned int i; // counter to loop from 1-10
    int item; // variable to hold random values
    BST b;
-
-   
 	b.root= NULL;  b.size=0;// tree initially empty
-for ( i = 1; i < argc; ++i ) { 
-      insertNode( &b, atoi(argv[i]));
+
+   for (i = 1; i < argc; ++i) { 
+      insertNode(&b, atoi(argv[i]));
    //  insertNode_R(&b.root,atoi(argv[i]) );
-  } // end for
+   } // end for
    // traverse the tree preOrder
    /* printf("\nThe preOrder traversal is:\n" );
    preOrder( rootPtr );*/
